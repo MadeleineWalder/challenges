055998e63fdda7739a131bce755eeb6762e074b9
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-06-09 12:01:52 +0000

    navbar form challenge

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
